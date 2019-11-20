a600f746d08190d20a9408c162895265fba4dc28
lb-1 <lb-1@MacBook-Air-Wojtek.local>
2019-11-20 18:33:22 +0100

    second version

--- run.py
+++ run.py
@@ -1,38 +1,51 @@
 import csv
 from collections import namedtuple
 
-Seps = namedtuple('seps', ['sep', 'decimal', 'thousands'])
+from arguments import argument_1, argument_2, argument_3, argument_4, argument_5
 
+Seps = namedtuple('seps', ['sep', 'decimal', 'thousands'])
 
 
 def guess_csv(argument=None):
     result_dict = {
         ';': 0,
         ',': 0,
-        '.': 0
+        '.': 0,
+        '|': 0
     }
-
-    for line in csv.reader(open(argument, "r")):
-        result_dict[';'] += line[0].count(';')
-        result_dict[','] += line[0].count(',')
-        result_dict['.'] += line[0].count('.')
-    # print(result_dict)
-
-    for key, value in result_dict.items():  # for name, age in dictionary.iteritems():  (for Python 2.x)
-        if value == max(result_dict.values()):
+    lines = argument.split('\n')
+    for key in result_dict.keys():
+        if lines[0].count(key):
             seps = key
-        elif value == min(result_dict.values()):
-            thousands = key
-        else:
-            decimal = key
 
-    result = Seps(seps, thousands, decimal)
-    # result = namedtuple('seps', (seps, decimal, thousands))
+    for line in lines[1:]:
+        if line:
+            rows = line.split(seps)
+            for row in rows:
+                result_dict[','] += row.count(',')
+                result_dict['.'] += row.count('.')
+    # print(result_dict)
+    decimal = None
+    thousands = None
+    for key, value in result_dict.items():
+        if key in [',', '.']:
+            if value == max(result_dict.values()):
+                decimal = key
+            elif value == min(result_dict.values()):
+                pass
+            else:
+                thousands = key
+
+    result = Seps(seps, decimal, thousands)
     return result
 
 
 
+
 print('start')
-print(guess_csv("input_1.csv"))
-print(guess_csv("input_2.csv"))
-print(guess_csv("input_3.csv"))
+print(guess_csv(argument_1))
+print(guess_csv(argument_2))
+print(guess_csv(argument_3))
+print(guess_csv(argument_4))
+print(guess_csv(argument_5))
+print('stop')
--- tests.py
+++ tests.py
@@ -1,24 +1,25 @@
 import unittest
 
+from arguments import argument_1, argument_2, argument_3, argument_4, argument_5
 from run import guess_csv, Seps
 
 
 class TestMethods(unittest.TestCase):
 
     def test_first_case(self):
-        argument = "input_1.csv"
-        # print(guess_csv(argument))
-        # print(Seps(';', ',', '.'))
-        self.assertEqual(guess_csv(argument), Seps(';', '.', ','))
+        self.assertEqual(guess_csv(argument_1), Seps(';', ',', '.'))
 
     def test_second_case(self):
-        argument = "input_2.csv"
-        self.assertEqual(guess_csv(argument), Seps(';', '.', ','))
+        self.assertEqual(guess_csv(argument_2), Seps(';', '.', ','))
 
     def test_third_case(self):
-        argument = "input_3.csv"
-        self.assertEqual(guess_csv(argument), Seps(';', '.', ','))
+        self.assertEqual(guess_csv(argument_3), Seps(';', '.', ','))
 
+    def test_fourth_case(self):
+        self.assertEqual(guess_csv(argument_4), Seps('|', ',', '.'))
+
+    def test_fifth_case(self):
+        self.assertEqual(guess_csv(argument_5), Seps(',', '.', None))
 
 if __name__ == '__main__':
     unittest.main()
